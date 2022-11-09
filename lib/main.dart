65959b4f0b92674d6dd7c575aa9dea3ac08b479c
tarek <mohiuddintarek2017@gmail.com>
2022-11-09 22:44:20 +0600

    your login page don't have any function for login or signup

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home:  LoginPage(),
--- lib/screens/signupscreen/tabbar view/nevegation_screen.dart
+++ lib/screens/signupscreen/tabbar view/nevegation_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:jadeais/screens/signupscreen/tabbar%20view/banijjo_screen.dart';
@@ -16,7 +14,7 @@ class NavegationBar extends StatefulWidget {
 }
 
 int selectedindex = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 final screen = [
   const HomePage(),
   const TradeScreen(),
@@ -29,29 +27,29 @@ class _NavegationBarState extends State<NavegationBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
+        bottomNavigationBar: CurvedNavigationBar(
           key: _bottomNavigationKey,
-        index: 0,
-        height: 60.0,
-        items: const [
-          Icon(Icons.add, size: 30),
-          Icon(Icons.list, size: 30),
-          Icon(Icons.compare_arrows, size: 30),
-          Icon(Icons.call_split, size: 30),
-          Icon(Icons.perm_identity, size: 30),
-        ],
-        color: Colors.white,
-        buttonBackgroundColor: Colors.white,
-        backgroundColor: const Color(0xffE5E5E5),
-        animationCurve: Curves.easeInOutCubicEmphasized,
-        animationDuration: const Duration(milliseconds: 600),
-        onTap: (index) {
-          setState(() {
-            selectedindex = index;
-          });
-        },
-        letIndexChange: (index) => true,
-      ),
-      body: screen[selectedindex]);
-      }
+          index: 0,
+          height: 60.0,
+          items: const [
+            Icon(Icons.add, size: 30),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.call_split, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: const Color(0xffE5E5E5),
+          animationCurve: Curves.easeInOutCubicEmphasized,
+          animationDuration: const Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              selectedindex = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),//login page don't work
+        body: screen[selectedindex]);
+  }
 }
